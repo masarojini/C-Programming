e69dda3909b078d38dfeadaec160946b836bd120
masarojini <40710857+masarojini@users.noreply.github.com>
2018-06-30 07:33:34 +0000

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%s",a);
+    if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u'))&&((a=='A')||(a=='E')||(a=='I')||(a='O')||(a=='U'))
+    {
+        printf("vowel");
+    }
+    else
+    {
+        printf("consonant"); 
+    }
+    return 0;
+}
