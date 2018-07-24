57ad6e7e70ee28220eb170897bbd6e7d5a0d09ed
masarojini <40710857+masarojini@users.noreply.github.com>
2018-07-24 09:57:29 +0000

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -6,9 +6,12 @@ int main()
     scanf("%c",&c);
 
     if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+    {
         printf("%c is an alphabet.",c);
+    }    
     else
+    {
         printf("%c is not an alphabet.",c);
-
+    }
     return 0;
 }
